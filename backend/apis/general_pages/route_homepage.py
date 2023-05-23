b49ccde4c6ee2972a84c639e63e189f1315fa135
moro23 <billhab66@gmail.com>
2023-05-23 18:52:31 +0000

    implemented api routes for the homepage.

---
+++ backend/apis/general_pages/route_homepage.py
@@ -0,0 +1,19 @@
+#route_homepage.py
+
+## lets import all modules
+from fastapi import APIRouter
+from fastapi import Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
+## lets create an object of Jinja2Templates
+## and instantiate it with director name
+templates = Jinja2Templates(directory="templates")
+
+## lets create an instance of APIRouter
+general_pages_router = APIRouter()
+
+
+@general_pages_router.get("/")
+async def home(request: Request):
+    return templates.TemplateResponse("general_pages/homepage.html", {"request": request})
