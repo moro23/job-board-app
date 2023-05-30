29703a36da0a83721686453f316b050c754bf704
moro23 <billhab66@gmail.com>
2023-05-30 11:50:34 +0000

    updated the main app to include the users route implementation

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,14 +2,23 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from core.config import settings
-from apis.general_pages.route_homepage import general_pages_router
+#from apis.general_pages.route_homepage import general_pages_router
+from apis.base import api_router
+from db.session import engine 
+from db.base import Base
+
 
 def include_router(app):
-    app.include_router(general_pages_router)
+    app.include_router(api_router)
 
 ## lets specify the dir to keep all static files
 def configure_static(app):
     app.mount("/static", StaticFiles(directory="static"), name="static")
+
+## lets create our tables 
+def create_tables():
+    Base.metadata.create_all(bind=engine)
+
 
 def start_application():
     ## lets create an instance of our fastapi app 
@@ -17,6 +26,7 @@ def start_application():
     app = FastAPI(title=settings.PROJECT_NAME, version=settings.PROJECT_VERSION)
     include_router(app)
     configure_static(app)
+    create_tables()
     return app
 
 app = start_application()
