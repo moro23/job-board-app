8fa90b7a53435d7f7d490a94f655d671ee3924a8
moro23 <billhab66@gmail.com>
2023-05-30 10:34:32 +0000

    added schemas for users.

---
+++ backend/schemas/users.py
@@ -0,0 +1,9 @@
+## schemas/users.py
+from typing import Optional
+from pydantic import BaseModel, EmailStr
+
+## properties required during user creation
+class UserCreate(BaseModel):
+    username : str 
+    email : EmailStr 
+    password : str
