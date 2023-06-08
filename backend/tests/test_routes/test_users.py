5d8d8d1c354a3a81d43ac03fb3413a43e6d9397a
moro23 <billhab66@gmail.com>
2023-06-08 13:12:54 +0000

    updated the test users file.

--- backend/tests/test_routes/test_users.py
+++ backend/tests/test_routes/test_users.py
@@ -11,3 +11,17 @@ def test_create_user(client):
     assert response.status_code == 200 
     assert response.json()["email"] == "testuser@nofoobar.com"
     assert response.json()["is_active"] == True
+
+# def test_read_user(client):
+#     data = {"username":"testuser",
+#     "email":"testuser@nofoobar.com",
+#     "password":"testing"}
+#     response = client.post("/users/",
+#     headers={"X-Token": "coneofsilence"},
+#     json=data
+    
+#     )
+
+#     response = client.get("/users/get/1/")
+#     assert response.status_code == 200 
+#     assert response.json()['email'] == "testuser@nofoobar.com"
