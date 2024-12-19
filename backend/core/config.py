d7af23321b36cc04ff4b8364ddad9726eda314ef
moro23 <billhab66@gmail.com>
2024-12-19 16:42:01 +0000

    updated config file.

--- backend/core/config.py
+++ backend/core/config.py
@@ -10,11 +10,11 @@ class Settings:
     PROJECT_NAME:str = "Job Board"
     PROJECT_VERSION:str = "1.0.0" 
  
-    POSTGRES_USER : str = os.getenv("POSTGRES_USER")
-    POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+    POSTGRES_USER : str = os.getenv("POSTGRES_USER", "postgres")
+    POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "password")
     POSTGRES_SERVER : str = os.getenv("POSTGRES_SERVER","localhost")
     POSTGRES_PORT : str = os.getenv("POSTGRES_PORT",5432) # default postgres port is 5432
-    POSTGRES_DB : str = os.getenv("POSTGRES_DB","tdd")
+    POSTGRES_DB : str = os.getenv("POSTGRES_DB","job_board_db")
     DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
 
     SECRET_KEY :str = os.getenv("SECRET_KEY")   #new
